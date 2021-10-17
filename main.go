48d67bb79d1e8fbe7f6d8ad1d467a74d979fad72
Brett <brettllapierre@gmail.com>
2021-10-17 16:27:08 -0700

    Changed unit price to float, name validation, and rounding single cent fractions
    Updated unit price type to be a float. If the user enters a 3rd value past the decimal point it will round it to the nearest cent. Added item name checking functionality with some helpful error text. This will ensure the item name is not blank.

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
+	"math"
 	"net/http"
 	"regexp"
 	"strings"
@@ -14,7 +15,7 @@ import (
 type Produce struct {
 	Name string `json:"name"`
 	Code string `json:"code"`
-	Unit_Price string `json:"unit_price"`
+	Unit_Price float64 `json:"unit_price"`
 }
 
 // Init produce var as a Produce slice
@@ -39,7 +40,6 @@ func getSingleProduce(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, fmt.Sprintf("The produce item %s does not exist.", params["name"]), 400)
 		}
 	}
-
 }
 
 // Add a new produce item
@@ -48,21 +48,21 @@ func addProduce(w http.ResponseWriter, r *http.Request) {
 	var newProduceItem Produce
 	_ = json.NewDecoder(r.Body).Decode(&newProduceItem)
 	re := regexp.MustCompile("^[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}$")
-	if re.MatchString(newProduceItem.Code) == true {
+	if re.MatchString(newProduceItem.Code) == true && len(newProduceItem.Name) > 0{
+		newProduceItem.Unit_Price = math.Round(newProduceItem.Unit_Price*100)/100 //rounds to the nearest cent
 		produce = append(produce, newProduceItem)
 		json.NewEncoder(w).Encode(newProduceItem)
 	} else {
-		http.Error(w, fmt.Sprintf("Incorrect produce code sequence. Example code: A12T-4GH7-QPL9-3N4M"), 400)
+		http.Error(w, fmt.Sprintf("Incorrect produce code sequence or product name. Example code sequence: A12T-4GH7-QPL9-3N4M"), 400)
 	}
-
 }
 
 func initializeProduceData(){
 	// Hardcoded data - @todo: add database
-	produce = append(produce, Produce{Name: "Lettuce", Code: "A12T-4GH7-QPL9-3N4M", Unit_Price: "3.46"})
-	produce = append(produce, Produce{Name: "Peach", Code: "E5T6-9UI3-TH15-QR88", Unit_Price: "2.99"})
-	produce = append(produce, Produce{Name: "Green Pepper", Code: "YRT6-72AS-K736-L4AR", Unit_Price: "0.79"})
-	produce = append(produce, Produce{Name: "Gala Apple", Code: "TQ4C-VV6T-75ZX-1RMR", Unit_Price: "3.59"})
+	produce = append(produce, Produce{Name: "Lettuce", Code: "A12T-4GH7-QPL9-3N4M", Unit_Price: 3.46})
+	produce = append(produce, Produce{Name: "Peach", Code: "E5T6-9UI3-TH15-QR88", Unit_Price: 2.99})
+	produce = append(produce, Produce{Name: "Green Pepper", Code: "YRT6-72AS-K736-L4AR", Unit_Price: 0.79})
+	produce = append(produce, Produce{Name: "Gala Apple", Code: "TQ4C-VV6T-75ZX-1RMR", Unit_Price: 3.59})
 }
 
 
