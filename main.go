233d1ef5e0875fd07a876c810365526678deb1a6
Brett <brettllapierre@gmail.com>
2021-11-28 17:41:26 -0800

    General code formatting changes and local address specification
    Made some formatting changes and specified my local testing address to resolve a security permission issue on my machine.

--- main.go
+++ main.go
@@ -3,18 +3,19 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"log"
 	"math"
 	"net/http"
 	"regexp"
 	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 // Define the produce structure
 type Produce struct {
-	Name string `json:"name"`
-	Code string `json:"code"`
+	Name       string  `json:"name"`
+	Code       string  `json:"code"`
 	Unit_Price float64 `json:"unit_price"`
 }
 
@@ -23,7 +24,6 @@ type ProduceList []Produce
 
 // global var where all produce is kept
 var produce ProduceList
-
 
 // Get all produce
 func getProduce(w http.ResponseWriter, r *http.Request) {
@@ -89,14 +89,13 @@ func deleteProduceItem(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(produce)
 }
 
-func initializeProduceData(){
+func initializeProduceData() {
 	// Hardcoded data - @todo: add database
 	produce = append(produce, Produce{Name: "Lettuce", Code: "A12T-4GH7-QPL9-3N4M", Unit_Price: 3.46})
 	produce = append(produce, Produce{Name: "Peach", Code: "E5T6-9UI3-TH15-QR88", Unit_Price: 2.99})
 	produce = append(produce, Produce{Name: "Green Pepper", Code: "YRT6-72AS-K736-L4AR", Unit_Price: 0.79})
 	produce = append(produce, Produce{Name: "Gala Apple", Code: "TQ4C-VV6T-75ZX-1RMR", Unit_Price: 3.59})
 }
-
 
 func main() {
 	// init router
@@ -109,5 +108,5 @@ func main() {
 	router.HandleFunc("/produce", addProduce).Methods("POST")
 	router.HandleFunc("/produce/{name}", deleteProduceItem).Methods("DELETE")
 	// Start server
-	log.Fatal(http.ListenAndServe(":8000", router))
+	log.Fatal(http.ListenAndServe("127.0.0.1:8000", router))
 }
